b7f3555226c82108a194178d131f16f8ae1821e2
masbung <masbung@sistrategia.com>
2017-05-25 13:12:29 -0500

    Adding minimal Katana/Owin infrastructure.
    - Adding Owin, Microsoft.Owin and Microsoft.Owin.Host.SystemWeb assemblies from NuGet.
    - Adding Startup class with OwinStartup assembly declaration.
    - Adding custom Run Method with hello world to check if infrastructure is working properly.

---
+++ src/MVC523FromScratch/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(MVC523FromScratch.Startup))]
+
+namespace MVC523FromScratch
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
